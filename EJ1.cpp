386faee4a2819318c6341ced40a5da3ab74fabfc
aalvaroo7 <almarrom@myuax.com>
2023-11-29 17:59:58 +0100

    ej1

--- EJ1.cpp
+++ EJ1.cpp
@@ -3,5 +3,8 @@
 #include <vector>
 using namespace std;
 
-vector<int>mivector ; //creo el vector
+vector<int>mivector ; //creo el vector mivector
 mivector.pushback(1); //agrego un elemento al vector
+mivector.pushback(2); //agrego otro elemento al vector
+mivector.pushback(3); //agrego otro elemento al vector
+mivector.pushback(4); //agrego otro elemento al vector
