a9753a73455fd8ba1d6acdbe7fc5c449327afa01
aalvaroo7 <almarrom@myuax.com>
2023-11-29 23:38:19 +0100

    comits ejercicios

--- EJ4.cpp
+++ EJ4.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 #include<valarray>
+#include<cmath>
 using namespace std;
 int main(){
-    valarray<int> v={6.6,4.5,4.7,5.6,8.9}
-    valarray<double> suma= v+10.0
-            valarray<double> resta= v-10.0
-            valarray<double> multiplicacion= v*10.0
-            valarray<double> division= v/10.0
-            cout<<"suma"<<endl;
+    valarray<int> lista={2.2,3.4,6.7,8.9,7.8,6.9}
+    valarray<double> suma= lista + 33.3;
+    valarray<double> resta= lista - 33.3;
+    valarray<double> multiplicacion= lista * 33.3;
+    valarray<double> division= lista / 33.3;
+    valarray<double> potencia= lista ** 33.3;
+    valarray<double> raiz= sqrt(lista);
+    valarray<double> logaritmo= log(lista);
+    valarray<double> seno= sin(lista);
+    valarray<double> coseno= cos(lista);
+    valarray<double> tangente= tan(lista);
+
 
 }
