d8ac2d332162269bd991359a0c7d7bb9c88c989c
aalvaroo7 <almarrom@myuax.com>
2023-11-29 23:34:35 +0100

    comits ejercicios

---
+++ EJ4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include<valarray>
+using namespace std;
+int main(){
+    valarray<int> v={6.6,4.5,4.7,5.6,8.9}
+    valarray<double> suma= v+10.0
+            valarray<double> resta= v-10.0
+            valarray<double> multiplicacion= v*10.0
+            valarray<double> division= v/10.0
+            cout<<"suma"<<endl;
+
+}
