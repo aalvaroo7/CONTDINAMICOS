e6c28a16144e79b33495b4a37aed695bf9a8f776
aalvaroo7 <almarrom@myuax.com>
2023-11-29 21:43:40 +0100

    ej1

--- EJ1.cpp
+++ EJ1.cpp
@@ -4,3 +4,11 @@
 using namespace std;
 int main(){
     cout << "Nombres" << endl;
+vector<string> personas;
+personas.push_back("Juan");
+personas.push_back("Pedro");
+personas.push_back("Maria");
+personas.push_back("Luis");
+personas.push_back("Ana");
+personas.push_back("Rosa");
+personas.push_back("Jose");
