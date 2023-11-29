ec5adb2e2b78e23be0b9d3c56cb0972f262e0fd2
aalvaroo7 <almarrom@myuax.com>
2023-11-29 23:53:36 +0100

    comits ejercicios

--- EJ5.cpp
+++ EJ5.cpp
@@ -3,10 +3,12 @@
 #include <algorithm>
 #include <memory>
 #include <functional>
+#include <cmath>
 using namespace std;
 int main(){
     auto logaritmo=[](double x){return log(x);};
-    auto seno=[](double x){return sin(x);};
     auto numeros = std::make_unique<std::vector<int>>(std::vector<int>{1, 2, 3, 4, 5});
-    std::for_each(numeros->begin(), numeros->end()
+    std::for_each(numeros->begin(), numeros->end(),[&logaritmo](int n){
+        std::cout << "el logaritmo es" <<logaritmo(n) << std::endl;
+    });
 }
