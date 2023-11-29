1a8dec6ef845d1b205621483b57dcf66cfa18ceb
aalvaroo7 <almarrom@myuax.com>
2023-11-29 21:49:16 +0100

    ej1

--- EJ1.cpp
+++ EJ1.cpp
@@ -14,6 +14,7 @@ personas.push_back("Rosa");
 personas.push_back("Jose");
 //con push_back añadimos elementos al final del vector
 vector<string>::iterator it;
+//definimos un iterador para recorrer el vector
 for(it = personas.begin(); it != personas.end(); it++){
     cout << *it << endl;
 }
@@ -27,3 +28,8 @@ numeros.push_back(5);
 numeros.push_back(6);
 //con push_back añadimos elementos al final del vector
 vector<int>::iterator it2;
+//definimos un iterador para recorrer el vector
+for(it2 = numeros.begin(); it2 != numeros.end(); it2++){
+    cout << *it2 << endl;
+
+}
