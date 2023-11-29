8c4c581564123e1287fab7d27bf57722baa7e620
aalvaroo7 <almarrom@myuax.com>
2023-11-29 21:30:14 +0100

    ej1

--- EJ1.cpp
+++ EJ1.cpp
@@ -1,24 +1,4 @@
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
-#include <algorithm>
-#include <iterator>
-#include <map>
-#include <set>
-#include <utility>
 #include <string>
 #include <vector>
 using namespace std;
-int main(){
-vector<int>mivector ; //creo el vector mivector
-mivector.push_back(1);//agrego un elemento al vector
-mivector.push_back(2); //agrego otro elemento al vector
-mivector.push_back(3); //agrego otro elemento al vector
-mivector.push_back(4); //agrego otro elemento al vector
-cout<<"El vector tiene componentes:";
-for(vector<int>::iterator i=mivector.begin(); i != mivector.end(),i++) { //recorro el vector
-    cout << *i << " ";//muestra los elementos
-}
-cout<<endl;
-return 0;
-}
