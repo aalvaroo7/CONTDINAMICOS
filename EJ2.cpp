75b7482bd8b6de3f3ab5441f299575381a158dd8
aalvaroo7 <almarrom@myuax.com>
2023-11-29 21:56:28 +0100

    ej1

--- EJ2.cpp
+++ EJ2.cpp
@@ -3,6 +3,16 @@
 #include <list>
 using namespace std;
 int main(){
-    list<float> lista;
-    lista.push_back(1.1);
+    list<float> lista{1.1,2.2,3.3,4.4,5.5};
+    stack<float, list<float>> pila(lista);
+    cout << "Elementos de la pila: " << endl;
+    while(!pila.empty()){
+        cout << pila.top() << endl;
+        pila.pop();
+    }
+    pila.push(6.6)
+    pila.push(7.7)
+    pila.push(8.8)
+    pila.push(9.9)
+    pila.push(10.10)
 }
