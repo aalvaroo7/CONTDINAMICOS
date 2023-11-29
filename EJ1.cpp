14dcfe7fd6367f0a0d778666346068395420e428
aalvaroo7 <almarrom@myuax.com>
2023-11-29 21:41:02 +0100

    ej1

--- EJ1.cpp
+++ EJ1.cpp
@@ -2,28 +2,3 @@
 #include <string>
 #include <vector>
 using namespace std;
-// Función para mostrar los elementos de un vector de strings
-void mostrarElementos(const vector<string>& elementos) {
-    for (const auto& elemento : elementos) {
-        cout << elemento << endl;
-    }
-}
-// Función para mostrar los elementos de un vector de enteros
-void mostrarElementos(const vector<int>& elementos) {
-    for (const auto& elemento : elementos) {
-        cout << elemento << endl;
-    }
-}
-int main() {
-    cout << "Nombres" << endl;
-    // Vector de nombres
-    vector<string> nombres = {"Juan", "Pedro", "Maria", "Jose", "Luis"};
-    // Llamada a la función para mostrar nombres
-    mostrarElementos(nombres);
-    cout << "Numeros" << endl;
-    // Vector de números
-    vector<int> numeros = {1, 2, 3, 4, 5};
-    // Llamada a la función para mostrar números
-    mostrarElementos(numeros);
-    return 0;
-}
