d508fa63f8e386b11d25033b2d8623cbf15c289d
aalvaroo7 <almarrom@myuax.com>
2023-11-29 18:05:40 +0100

    ej1

--- EJ1.cpp
+++ EJ1.cpp
@@ -2,9 +2,17 @@
 #include <string>
 #include <vector>
 using namespace std;
-
+int main(){
 vector<int>mivector ; //creo el vector mivector
-mivector.pushback(1); //agrego un elemento al vector
+mivector.pushback(1);//agrego un elemento al vector
 mivector.pushback(2); //agrego otro elemento al vector
 mivector.pushback(3); //agrego otro elemento al vector
-mivector.pushback(4); //agrego otro elemento al vector
+mivector_pushback(4); //agrego otro elemento al vector
+cout<<"El vector tiene componentes:"
+for(vector<int>::iterator i=mivector.begin())
+
+
+
+
+return 0;
+}
