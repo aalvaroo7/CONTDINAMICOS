0a09035b96daf4efb47ce36e2988f690a0a50ea8
aalvaroo7 <almarrom@myuax.com>
2023-11-29 18:39:27 +0100

    ej1

--- EJ2.cpp
+++ EJ2.cpp
@@ -1,14 +1,14 @@
-
+//el codigo de ruben incluye solo el metodo pop y top , debemos incluir push
 #include <iostream>
 #include <list>
 #include <stack>
-
+using namespace std;
     int main() {
-        std::list<float> lista = {1.1, 2.2, 3.3, 4.4, 5.5};
-        std::stack<float, std::list<float>> pila(lista);
+        list<float> lista = {1.1, 2.2, 3.3, 4.4, 5.5};
+        stack<float, std::list<float>> pila(lista);
 
         while(!pila.empty()) {
-            std::cout << pila.top() << std::endl;
+            cout << pila.top() << std::endl;
             pila.pop();
         }
 
