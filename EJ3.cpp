ea7745d7ea586981aa1af04c325d2179c2f76c7b
aalvaroo7 <almarrom@myuax.com>
2023-11-29 23:30:04 +0100

    comits ejercicios

--- EJ3.cpp
+++ EJ3.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 #include<vector>
-#include<algorithym>
+#include<algorithm>
 using namespace std;
 int main(){
     vector<int>V={9,4,2,6,8,3};
@@ -8,4 +8,9 @@ int main(){
     for(int i=0;i<V.size();i++){
         cout<<V[i]<<endl;
     }
+    auto elementomaximo= max_element(V.begin(),V.end());
+    cout<<"el elemento maximo es: "<<*elementomaximo<<endl;
+    auto elementominimo= min_element(V.begin(),V.end());
+    cout<<"el elemento minimo es: "<<*elementominimo<<endl;
+    return 0;
 }
