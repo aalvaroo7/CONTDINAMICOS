fa3d99a5974deb8072cef1bef3647b117f2aef5e
aalvaroo7 <almarrom@myuax.com>
2023-11-29 21:46:02 +0100

    ej1

--- EJ1.cpp
+++ EJ1.cpp
@@ -17,3 +17,11 @@ vector<string>::iterator it;
 for(it = personas.begin(); it != personas.end(); it++){
     cout << *it << endl;
 }
+cout << "numeros" << endl;
+vector<int> numeros;
+numeros.push_back(1);
+numeros.push_back(2);
+numeros.push_back(3);
+numeros.push_back(4);
+numeros.push_back(5);
+numeros.push_back(6);
